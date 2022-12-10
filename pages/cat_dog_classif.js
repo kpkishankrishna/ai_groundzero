ef13b6b622a89549a6eafa2cfa23095b8f182256
kpkishankrishna <41905353+kpkishankrishna@users.noreply.github.com>
2022-12-10 23:37:28 +0530

    Working cat dog classifier

--- pages/cat_dog_classif.js
+++ pages/cat_dog_classif.js
@@ -3,89 +3,37 @@ import { useEffect, useState } from 'react';
 
 
 const catDogClassif = () =>  {
-  const imageMimeType = /image\/(png|jpg|jpeg)/i;
-  const [file, setFile] = useState(null);
-  const [fileDataURL, setFileDataURL] = useState(null);
-  const [label, setLabel] = useState(null)
-
-  const changeHandler = (e) => {
-    const file = e.target.files[0];
-    if (!file.type.match(imageMimeType)) {
-      alert("Image mime type is not valid");
-      return;
-    }
-    setFile(file);
-    setLabel('Loading')
-  }
-
   async function loaded(reader) {
     const response = await fetch('https://krushna-space1.hf.space/api/predict', {
-      method: "POST", body: JSON.stringify({ "data": [reader] }),
+      method: "POST", body: JSON.stringify({ "data": [reader.result] }),
       headers: { "Content-Type": "application/json" }
     });
+    console.log(reader.result)
     const json = await response.json();
-    setLabel(json['data'][0]['confidences'][0]['label'])
-    console.log(123)
+    const label = json['data'][0]['confidences'][0]['label'];
+    results.innerHTML = `<br/><img src="${reader.result}" width="300"> <p>${label}</p>`
   }
-
-  useEffect(() => {
-    console.log(123)
-    let fileReader, isCancel = false;
-    if (file) {
-      fileReader = new FileReader();
-      fileReader.onload = (e) => {
-        const { result } = e.target;
-        if (result && !isCancel) {
-          setFileDataURL(result)
-        }
-      }
-      fileReader.readAsDataURL(file);
-      loaded(fileDataURL)
+  function read() {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => loaded(reader))
+    reader.readAsDataURL(photo.files[0]);
+  }
+    const changeHandler = (e) => {
+      const reader = new FileReader();
+      reader.addEventListener('load', () => loaded(reader))
+      reader.readAsDataURL(photo.files[0]);
     }
-    if (fileDataURL) {
-      console.log(1234)
-      const temp = fileDataURL
-      console.log(temp)
-      loaded(temp)
-    }
-    return () => {
-      isCancel = true;
-      if (fileReader && fileReader.readyState === 1) {
-        fileReader.abort();
-      }
-    }
-
-  }, [file]);
 
   return (
     <>
-      <div>..</div>
-      <div>..</div>
-      <div>..</div>
-      <div>..</div>
-      <form>
-        <p>
-          <label htmlFor='image'> Browse images  </label>
-          <input
-            type="file"
-            id='image'
-            accept='.png, .jpg, .jpeg'
-            onChange={changeHandler}
-          />
-        </p>
-        <p>
-          <input type="submit" label="Upload" />
-        </p>
-      </form>
-      {fileDataURL ?
-        <div className="img-preview-wrapper">
-          {
-            <img src={fileDataURL} alt="preview" />
-          }
-          <h1>{label}</h1>
-        </div> : null}
+      <div class="p-5 bg-primary text-white text-center">
+        <h1>Dog Cat Classifier</h1>
+        {/* <p>Give an image of cat or a dog</p>  */}
+      </div>
+      <input id="photo" type="file" class="text-center"  onChange={changeHandler} />
+      <div id="results" class="p-5 bg-primary text-white text-center"></div>
     </>
-  );
+  )
 }
 
 export default catDogClassif
